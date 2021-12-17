a510e222cb2823d62351bbb6b62588e883d6dd37
ranmses <ranmses@gmail.com>
2021-12-17 20:31:42 -0300

    Mutiple fixes and logging functionality

--- pre_rpmbuild.sh
+++ pre_rpmbuild.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 [[ ! -z "$1" ]] && hvrs="$1" || { echo "Please inform the desired Harmony version e.g. '$0 4.3.0'"; exit 1; }
-yum install -y git --quiet
-yum groupinstall -y "RPM Development Tools" --quiet
+exec > >(tee "${0}.log") 2>&1
+date
+gdir=$(pwd)
+yum install -y golang which git gcc-c++ glibc-devel gmp-devel make openssl-devel
+yum groupinstall -y "RPM Development Tools"
 rpmdev-setuptree
 sdir=/root/rpmbuild/SOURCES/
 cd ${sdir}
 harm="harmony-one-${hvrs}/go/src/github.com/harmony-one"
 rm -rf ${harm}
 mkdir -p ${harm} && cd ${harm}
-(
 git clone --quiet "https://github.com/harmony-one/mcl.git"
 git clone --quiet "https://github.com/harmony-one/bls.git"
 git clone --quiet "https://github.com/harmony-one/harmony.git" --branch "v${hvrs}"
-) &> /dev/null
 cd harmony
-# go version requirement and repo release
 gvrs=$(egrep "^go" go.mod  | cut -d' ' -f2)
 hrel=$(git describe --long | cut -d'-' -f2)
-# spec file
 spec=/root/rpmbuild/SPECS/harmony-one-${hvrs}.spec
-cp -f /images/gc-b71/harmony-one.spec.template ${spec}
+cp -f ${gdir}/harmony-one.spec.template ${spec}
 sed -i "s/HVRS/$hvrs/g" ${spec}
 sed -i "s/HREL/$hrel/g" ${spec}
 sed -i "s/GVRS/$gvrs/g" ${spec}
